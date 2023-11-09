205b5cbe6866674cabfb1d591bb4973fd0babfa6
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-11-09 17:08:09 +0100

    nb of step to reduce to zero

---
+++ step_nb_to_reduce_to_zero.go
@@ -0,0 +1,48 @@
+package main
+
+// This is a simulation problem;
+// all we have to do to arrive at the correct result is follow the instructions.
+// Through this problem, you will learn how to identify if a number is even or odd
+// (hint, think about the modulo operator from the previous problem).
+// For those who are looking for an added challenge,
+// you can try solving this problem using bitwise operations.
+// Both methods are introduced in the video solution.
+// We encourage you to try solving 1342.
+// Number of Steps to Reduce a Number to Zero on your own first,
+// then return here to watch the video solution.
+//
+// time complexity: O(log(n))
+// space complexity: O(1)
+func numberOfSteps(num int) int {
+	var stepNb int
+	for stepNb = 0; num > 0; stepNb++ {
+		if num%2 == 0 {
+			num /= 2
+			continue
+		}
+
+		num--
+	}
+
+	return stepNb
+}
+
+func numberOfSteps_bit(num int) int {
+	var stepNb int
+	if num == 0 {
+		return 0
+	}
+
+	for num > 0 {
+		// is odd
+		if num&1 == 1 {
+			num -= 1
+			stepNb++
+		}
+
+		num >>= 1
+		stepNb++
+	}
+
+	return stepNb - 1
+}
---
+++ step_nb_to_reduce_to_zero_test.go
@@ -0,0 +1,68 @@
+package main
+
+import "testing"
+
+var testCases = []struct {
+	name string
+	num  int
+	want int
+}{
+	{
+		name: "example 1",
+		num:  14,
+		want: 6,
+	},
+	{
+		name: "example 2",
+		num:  8,
+		want: 4,
+	},
+	{
+		name: "example 3",
+		num:  123,
+		want: 12,
+	},
+	{
+		name: "zero",
+		num:  0,
+		want: 0,
+	},
+}
+
+func Test_numberOfSteps(t *testing.T) {
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := numberOfSteps(tt.num); got != tt.want {
+				t.Errorf("numberOfSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_numberOfSteps_bit(t *testing.T) {
+	for _, tt := range testCases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			if got := numberOfSteps_bit(tt.num); got != tt.want {
+				t.Errorf("numberOfSteps() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_numberOfSteps(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		numberOfSteps(123)
+	}
+}
+
+func Benchmark_numberOfSteps_bit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		numberOfSteps_bit(123)
+	}
+}
