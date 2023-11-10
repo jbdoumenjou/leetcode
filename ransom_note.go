8c44a75d3eac9209907b00de33a7b2c3fefe2ad2
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-11-10 11:43:34 +0100

    ransom note

---
+++ ransom_note.go
@@ -0,0 +1,48 @@
+package main
+
+// Given two strings ransomNote and magazine,
+// return true if ransomNote can be constructed by using the letters from magazine
+// and false otherwise.
+//
+// Each letter in magazine can only be used once in ransomNote.
+//
+// Time complexity: O(n)
+// Space complexity: O(1)
+func canConstruct(ransomNote string, magazine string) bool {
+	letters := make(map[rune]int)
+	for _, r := range ransomNote {
+		letters[r] += 1
+	}
+
+	availableLetters := make(map[rune]int)
+	for _, r := range magazine {
+		availableLetters[r] += 1
+	}
+
+	for r, n := range letters {
+		if availableLetters[r] < n {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+func canConstruct_array(ransomNote string, magazine string) bool {
+	var letterCount [26]int
+
+	for _, r := range magazine {
+		letterCount[r-'a'] += 1
+	}
+
+	for _, r := range ransomNote {
+		letterCount[r-'a'] -= 1
+		if letterCount[r-'a'] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ ransom_note_test.go
@@ -0,0 +1,79 @@
+package main
+
+import "testing"
+
+var canConstructTests = []struct {
+	name       string
+	ransomNote string
+	magazine   string
+	want       bool
+}{
+	{
+		name:       "example 1",
+		ransomNote: "a",
+		magazine:   "b",
+		want:       false,
+	},
+	{
+		name:       "example 2",
+		ransomNote: "aa",
+		magazine:   "ab",
+		want:       false,
+	},
+	{
+		name:       "example 3",
+		ransomNote: "aa",
+		magazine:   "aab",
+		want:       true,
+	},
+	{
+		name:       "example 4",
+		ransomNote: "bg",
+		magazine:   "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj",
+		want:       true,
+	},
+	{
+		name:       "example 5",
+		ransomNote: "aab",
+		magazine:   "baa",
+		want:       true,
+	},
+	{
+		name:       "example 6",
+		ransomNote: "fffbfg",
+		magazine:   "effjfggbffjdgbjjhhdegh",
+		want:       true,
+	},
+}
+
+func Test_canConstruct(t *testing.T) {
+	for _, tt := range canConstructTests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canConstruct(tt.ransomNote, tt.magazine); got != tt.want {
+				t.Errorf("canConstruct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_canConstruct_array(t *testing.T) {
+	for _, tt := range canConstructTests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canConstruct_array(tt.ransomNote, tt.magazine); got != tt.want {
+				t.Errorf("canConstruct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_canConstruct(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		canConstruct(canConstructTests[0].ransomNote, canConstructTests[0].magazine)
+	}
+}
+
+func Benchmark_canConstruct_array(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		canConstruct_array(canConstructTests[0].ransomNote, canConstructTests[0].magazine)
+	}
+}
